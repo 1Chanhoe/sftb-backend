528a1af8a665e779773551038ea14c83f279d7d8
seosujin <sytolo020912@gmail.com>
2024-09-03 13:48:07 +0900

    sujin

--- sftb/src/main/java/com/example/demo/repository/UserMapper.java
+++ sftb/src/main/java/com/example/demo/repository/UserMapper.java
@@ -1,11 +1,29 @@
 package com.example.demo.repository;
-//Mybatis를 사용해 데베와 상호작용하는 인터페이스 부분
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import com.example.demo.entity.User;
+import org.apache.ibatis.annotations.Update;
 
-@Mapper //Mybatis가 이 인터페이스를 매퍼로 인식하게 함
+@Mapper
 public interface UserMapper {
-    @Insert("INSERT INTO customers (UserID, StudentID, Password, UserName, Email) VALUES (#{userID}, #{studentID}, #{password}, #{userName}, #{email})") //SQL INSERT문 정의
-    void insertUser(User user); //User객체를 데베에 삽입
+    @Insert("INSERT INTO customers (UserID, StudentID, Password, UserName, Email) VALUES (#{userID}, #{studentID}, #{password}, #{userName}, #{email})")
+    void insertUser(User user);
+
+    @Select("SELECT * FROM customers WHERE UserID = #{userID}")
+    User findByUserId(@Param("userID") String userID);
+    
+    @Select("SELECT * FROM customers WHERE UserName = #{userName} AND Email = #{email}")
+    User findByNameAndEmail(@Param("userName") String userName, @Param("email") String email);
+    
+    @Select("SELECT * FROM customers WHERE UserName = #{userName} AND Email = #{email} AND UserID = #{userID}")
+    User findByNameEmailAndId(@Param("userName") String userName, @Param("email") String email, @Param("userID") String userID);
+    
+    @Update("UPDATE customers SET Password = #{password} WHERE UserID = #{userID}")
+    void updateUser(User user);
+    
+    
+
 }
