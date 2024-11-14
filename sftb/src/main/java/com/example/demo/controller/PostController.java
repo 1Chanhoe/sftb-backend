f5e7a1eeaf6a9c42199aea78eb71b23be476fd8e
weon1013 <x147x147@naver.com>
2024-11-14 21:25:59 +0900

    11/14 성원 커밋용

--- sftb/src/main/java/com/example/demo/controller/PostController.java
+++ sftb/src/main/java/com/example/demo/controller/PostController.java
@@ -32,17 +32,9 @@ public class PostController {
     @PostMapping
     public ResponseEntity<?> createPost(@RequestBody PostRequest postRequest) 
     {
-        Post post = new Post();
-        post.setTitle(postRequest.getTitle());
-        post.setUserName(postRequest.getUserName()); // Member_ID를 userName으로 사용
-        post.setContent(postRequest.getContent());
-        post.setBoardId(postRequest.getBoardId()); // boardId 설정
-        post.setViewCount(0); // 초기 조회수 설정
-        post.setUserId(postRequest.getUserId());
+    	Post post = postRequest.toPost();
         postService.createPost(post);
         return ResponseEntity.ok(post);
-
-
     }
     // 게시물 목록 가져오기 (Board_ID로 필터링)
     @GetMapping
